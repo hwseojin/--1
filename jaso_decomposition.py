9c888099f18edbbe25b5f68166ce6f7f0ade2f10
Simhyeone <84944686+Simhyeone@users.noreply.github.com>
2021-06-01 10:53:47 +0900

    선생님 코딩

---
+++ jaso_decomposition.py
@@ -0,0 +1,102 @@
+#1# -*- coding: utf-8 -*-
+import re
+import sys
+"""
+        초성 중성 종성 분리 하기
+	유니코드 한글은 0xAC00 으로부터
+	초성 19개, 중성21개, 종성28개로 이루어지고
+	이들을 조합한 11,172개의 문자를 갖는다.
+	한글코드의 값 = ((초성 * 21) + 중성) * 28 + 종성 + 0xAC00
+	(0xAC00은 'ㄱ'의 코드값)
+	따라서 다음과 같은 계산 식이 구해진다.
+	유니코드 한글 문자 코드 값이 X일 때,
+	초성 = ((X - 0xAC00) / 28) / 21
+	중성 = ((X - 0xAC00) / 28) % 21
+	종성 = (X - 0xAC00) % 28
+	이 때 초성, 중성, 종성의 값은 각 소리 글자의 코드값이 아니라
+	이들이 각각 몇 번째 문자인가를 나타내기 때문에 다음과 같이 다시 처리한다.
+	초성문자코드 = 초성 + 0x1100 //('ㄱ')
+	중성문자코드 = 중성 + 0x1161 // ('ㅏ')
+	종성문자코드 = 종성 + 0x11A8 - 1 // (종성이 없는 경우가 있으므로 1을 뺌)
+"""
+# 유니코드 한글 시작 : 44032, 끝 : 55199
+BASE_CODE, CHOSUNG, JUNGSUNG = 44032, 588, 28
+
+# 초성 리스트. 00 ~ 18
+CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+# 중성 리스트. 00 ~ 20
+JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+
+# 종성 리스트. 00 ~ 27 + 1(1개 없음)
+JONGSUNG_LIST = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+result = list()
+uni_list = list()
+
+def convert(test_keyword):
+    split_keyword_list = list(test_keyword)
+    #print(split_keyword_list)
+
+    for keyword in split_keyword_list:
+        # 한글 여부 check 후 분리
+        if re.match('.*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*', keyword) is not None:
+            uni_list.append(ord(keyword))
+            char_code = ord(keyword) - BASE_CODE
+            char1 = int(char_code / CHOSUNG)
+            result.append(CHOSUNG_LIST[char1])
+            #print('초성 : {}'.format(CHOSUNG_LIST[char1]))
+            char2 = int((char_code - (CHOSUNG * char1)) / JUNGSUNG)
+            result.append(JUNGSUNG_LIST[char2])
+            #print('중성 : {}'.format(JUNGSUNG_LIST[char2]))
+            char3 = int((char_code - (CHOSUNG * char1) - (JUNGSUNG * char2)))
+            if char3==0:
+                result.append('#')
+            else:
+                result.append(JONGSUNG_LIST[char3])
+            #print('종성 : {}'.format(JONGSUNG_LIST[char3]))
+        else:
+            result.append(keyword)
+    # result
+#   print("".join(result))
+
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) > 1:
+        inputfile = open(sys.argv[1], 'r')
+        for line in inputfile.readlines():
+            convert(line)
+    else:
+        test_keyword = input("input your text:")
+
+        convert(test_keyword)
+        print( result )
+
+        result_keyword = list(test_keyword)
+
+        # 음운의 변동 탈락 - 자음 탈락 - ㄹ 탈락
+        
+        for i in range(2, len(result), 3):
+            if result[i] == "ㄻ" :
+                result[i] = "ㅁ"
+                addr = int((i-2)/3)
+                result_char = int(uni_list[addr])-8
+                print( result_keyword )
+                result_keyword[addr] = chr(result_char)
+                
+            if result[i] == "ㄺ" :
+                result[i] = "ㄱ"
+                addr = int((i-2)/3)
+                result_char = int(uni_list[addr])-8
+                print( result_keyword )
+                result_keyword[addr] = chr(result_char)
+            if result[i] == "ㅌ":
+                if result[i+1] == "ㅇ":
+                    addr = int((i-2)/3)
+                    result_char = int(uni_list[addr])-25
+                    result_keyword[addr] = chr(result_char)
+                    result_char = int(uni_list[addr])+3*21*29+2500
+                    result_keyword[addr+1] = chr(result_char)
+
+print( result_keyword )
